8056311a306330f74dd9ad2b49a0bcf231790716
Newton Manyisa <manyisanewton26@gmail.com>
2024-12-18 08:43:16 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Write your code here!
+// Remove the <main> element with id "main"
+const main = document.getElementById("main");
+main.remove();
+
+// Create a new <h1> element
+const newHeader = document.createElement("h1");
+
+// Set the id of the new <h1> element
+newHeader.id = "victory";
+
+// Set the text content of the new <h1> element
+newHeader.textContent = "Newton is the champion";
+
+// Append the new <h1> element to the document body
+document.body.append(newHeader);
